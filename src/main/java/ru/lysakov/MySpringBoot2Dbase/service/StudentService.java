02edc521f0d4e990ed540dd75cefaafecccbc46f
MakarLysakov <makarkalisakov@gmail.com>
2023-11-27 00:05:54 +0500

    initial commit

---
+++ src/main/java/ru/lysakov/MySpringBoot2Dbase/service/StudentService.java
@@ -0,0 +1,16 @@
+package ru.lysakov.MySpringBoot2Dbase.service;
+
+import org.springframework.stereotype.Service;
+import ru.lysakov.MySpringBoot2Dbase.entity.Student;
+
+import java.util.List;
+@Service
+public interface StudentService {
+    List<Student> getAllStudents();
+
+    Student saveStudent(Student student);
+
+    Student getStudent(int id);
+
+    void deleteStudent(int id);
+}
